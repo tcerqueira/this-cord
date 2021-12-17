584842a5fe40009be8eec9b625b1ebd75c498c37
tcerqueira <tiago.cerqueira7777@gmail.com>
2021-12-17 18:37:21 +0000

    start frontend

---
+++ app/r/home.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include "../components/head.php" ?>
+</head>
+<body>
+
+    <?php include "../components/nav-primary.php" ?>
+
+</body>
+</html>
