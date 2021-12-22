4f983a9ed3b0b4b214470401149bf1f59c016e51
tcerqueira <tiago.cerqueira7777@gmail.com>
2021-12-22 02:56:56 +0000

    irrelevant changes

--- app/components/nav-primary.php
+++ app/components/nav-primary.php
@@ -1,6 +1,6 @@
 <nav class="primary-nav">
     <a href="#">
-        <div class="side-card" data-tooltip="Home">
+        <div class="side-card" data-tooltip="Home" data-server-selected>
             <span>Home</span>
             <!-- <span class="tooltip">Home</span> -->
         </div>
@@ -9,7 +9,7 @@
     <div id="guilds-container" class="guilds-container">
 
     </div>
-    <div class="side-card" data-tooltip="Create Server" data-server-selected>
+    <div class="side-card" data-tooltip="Create Server">
         +
     </div>
 </nav>
