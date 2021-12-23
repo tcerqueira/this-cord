89f7da650e2b15a77f024bbebf65120ec2fc3e56
tcerqueira <tiago.cerqueira7777@gmail.com>
2021-12-23 20:35:02 +0000

    render message author in progress

--- app/javascript/text-channel.js
+++ app/javascript/text-channel.js
@@ -1,29 +1,34 @@
 const messagesList = document.getElementById("messages-list");
-console.log(messagesList);
 
 const messages = [
-    { id: 1, content: "Hellommmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm"},
-    { id: 1, content: "Hello"},
-    { id: 1, content: "Hello"},
-    { id: 1, content: "Hello"},
-    { id: 1, content: "Hello"},
-    { id: 1, content: "Hello"},
-    { id: 1, content: "Hello"},
-    { id: 1, content: "Hello"},
-    { id: 1, content: "Hello"},
-    { id: 1, content: "Hello"},
-    { id: 1, content: "Hello"},
-    { id: 1, content: "Hello"},
-    { id: 1, content: "Hello"},
-    { id: 1, content: "Hello"},
-    { id: 1, content: "Hello"},
-    { id: 1, content: "Hello"},
-    { id: 1, content: "Hello"},
-    { id: 1, content: "Hello"}
+    { id: 1, author: 'lou', content: "Hellommmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm"},
+    { id: 1, author: 'lou', content: "Hello"},
+    { id: 1, author: 'lou', content: "Hello"},
+    { id: 1, author: 'lou', content: "Hello"},
+    { id: 1, author: 'lou', content: "Hello"},
+    { id: 1, author: 'lou', content: "Hello"},
+    { id: 1, author: 'lou', content: "Hello"},
+    { id: 1, author: 'tansi', content: "Hello"},
+    { id: 1, author: 'lou', content: "Hello"},
+    { id: 1, author: 'lou', content: "Hello"},
+    { id: 1, author: 'lou', content: "Hello"},
+    { id: 1, author: 'lou', content: "Hello"},
+    { id: 1, author: 'burro', content: "Hello"},
+    { id: 1, author: 'lou', content: "Hello"},
+    { id: 1, author: 'lou', content: "Hello"},
+    { id: 1, author: 'lou', content: "Hello"},
+    { id: 1, author: 'lou', content: "Hello"},
+    { id: 1, author: 'lou', content: "Hello"}
 ];
 
+let lastMessage = undefined;
 messages.forEach(message => {
     renderMessage(message);
+    if(lastMessage && lastMessage.author != message.author)
+    {
+        renderMessageAuthor(lastMessage.author);
+    }
+    lastMessage = message;
 });
 
 const replyIcons = document.querySelectorAll('.reply-message-icon');
@@ -39,7 +44,7 @@ function renderMessage(message)
     const listItem = document.createElement('li');
     listItem.classList.add('message');
     listItem.innerText = message.content;
-    renderMessageOptions(listItem, false);
+    renderMessageOptions(listItem, true);
     messagesList.append(listItem);
 }
 
@@ -52,5 +57,21 @@ function renderMessageOptions(listItem, deletable)
     replyIcon.src = "../public/reply-svgrepo-com.svg";
     replyIcon.alt = 'reply-icon';
     div.append(replyIcon);
+    if(deletable)
+    {
+        const removeIcon = document.createElement('img');
+        removeIcon.src = "../public/trash-svgrepo-com.svg";
+        removeIcon.alt = 'remove-icon';
+        div.append(removeIcon);
+    }
     listItem.append(div);
+}
+
+function renderMessageAuthor(author)
+{
+    console.log(author);
+    const authorAvatar = document.createElement('div');
+    authorAvatar.classList.add('author-avatar');
+    authorAvatar.innerText = author;
+    messagesList.append(authorAvatar);
 }
