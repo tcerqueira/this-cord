939215b5f8b10ed16f196f6792e192c4af90dea6
tcerqueira <tiago.cerqueira7777@gmail.com>
2021-12-27 17:15:25 +0000

    create and get guilds

--- api/controllers/GuildController.php
+++ api/controllers/GuildController.php
@@ -1,18 +1,26 @@
 <?php
 namespace controllers;
 use gateways\GuildGateway;
+use GuzzleHttp\Psr7\Response;
 
 class GuildController
 {
     private $db;
     private $guildGateway;
-    private $id;
 
-    public function __construct($db, $guild_id)
+    public function __construct($db)
     {
         $this->db = $db;
         $this->guildGateway = new GuildGateway($db);
-        $this->id = $guild_id;
+    }
+
+    public function getGuild($id, $user_id)
+    {
+        $result = $this->guildGateway->find($id);
+        $result = pg_fetch_assoc($result);
+        $response['status_code_header'] = 'HTTP/1.1 200 OK';
+        $response['body'] = json_encode($result);
+        return $response;
     }
 
     public function getGuildsByUser($user_id)
@@ -20,42 +28,56 @@ class GuildController
 
     }
 
-    public function getUserRoles($user_id)
+    public function getUserRoles($id, $user_id)
     {
 
     }
 
     public function createGuild($user_id, $input)
     {
-
+        $result = $this->guildGateway->insert([
+            $input['guildname'],
+            $input['initials'],
+            $user_id,
+            $input['open_invite_key'],
+            $input['theme_color']
+        ]);
+        if(!$result)
+        {
+            $response = internalServerErrorResponse();
+            return $response;
+        }
+        $result = pg_fetch_assoc($result);
+        $response = okResponse(['guild_id' => $result['id']]);
+        return $response;
     }
 
-    public function updateGuild($input)
+    public function updateGuild($id, $input)
     {
 
     }
 
-    public function deleteGuild()
+    public function deleteGuild($id, $user_id)
     {
 
     }
 
-    public function getMembers()
+    public function getMembers($id)
     {
         
     }
 
-    public function inviteMember($added_user_id, $invite_sender_id)
+    public function inviteMember($id, $added_user_id, $invite_sender_id)
     {
 
     }
 
-    public function kickMember($member_id)
+    public function kickMember($id, $member_id, $requester_id)
     {
 
     }
 
-    public function updateMemberRole($member_id)
+    public function updateMemberRole($id, $member_id, $requester_id)
     {
 
     }
--- api/gateways/GuildGateway.php
+++ api/gateways/GuildGateway.php
@@ -9,6 +9,32 @@ class GuildGateway
         $this->db = $db;
     }
 
-    
+    public function find($id)
+    {
+        $query = "SELECT id,guildname,initials,admin_id,theme_color ".
+                 "FROM guild ".
+                 "WHERE id=$1;";
+        $result = pg_query_params($this->db, $query, [$id]);
+        return $result;
+    }
+
+    public function findMembers($id)
+    {
+
+    }
+
+    public function findAllOfMember($member_id)
+    {
+
+    }
+
+    public function insert($input)
+    {
+        $query = "INSERT INTO guild (guildname, initials, admin_id, open_invite_key, theme_color) ".
+                 "VALUES ($1, $2, $3, $4, $5) ".
+                 "RETURNING id;";
+        $result = pg_query_params($this->db, $query, $input);
+        return $result;
+    }
 }
 ?>
