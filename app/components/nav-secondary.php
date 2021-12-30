65f42084a66aec8a200386f00e8df1f9e1a8465c
PedroAidos <up201706842@fe.up.pt>
2021-12-30 19:10:29 +0000

    nav-secondary fix some space

--- app/components/nav-secondary.php
+++ app/components/nav-secondary.php
@@ -28,22 +28,21 @@
                 <h2> Voice channels </h2>
                 <a href="#"><img src="../public/add_button.svg" alt="add_button"> </a>
             </div>
-            <ul>
-                <li class="text-channel">
+            <ul id="voice-channels-list">
+                <!-- <li class="text-channel">
                     <a href="#"><span>voice channel 1</span></a>
                     <div>
                         <a href="#"> <img src="../public/add_user.svg" alt="add_user"></a>
                         <a href="#"><img src="../public/settings.svg" alt=""></a>
                     </div>
-                </li>
+                </li> -->
             </ul>
         </section>
     </div>
     
-    <div class="user">
+    <div class="user shadow" >
         <a href="#"><img src="../public/user.svg" alt="user"></a>
         <span>user name</span>
-        <span> # user id</span>
         <a href="#"><img src="../public/settings.svg" alt=""></a>
     </div>
 </nav>
--- app/javascript/secondary-nav.js
+++ app/javascript/secondary-nav.js
@@ -1,13 +1,24 @@
 const textChannelList = document.getElementById('text-channels-list');
+const voiceChannelList = document.getElementById('voice-channels-list');
 
 const allTextChannels = [
     {id: 1, name: 'jardim-de-convivio-da-feup'},
     {id: 2, name: 'spam'}
 ];
 
+const allVoiceChannels = [
+    {id: 1, name: 'robot'},
+    {id: 2, name: 'business'}
+];
+
 allTextChannels.forEach(textChannel => {
     const textChannelItem = renderTextChannel(textChannel);
     textChannelList.append(textChannelItem);
+});
+
+allVoiceChannels.forEach(voiceChannel => {
+    const voiceChannelItem = renderVoiceChannel(voiceChannel);
+    voiceChannelList.append(voiceChannelItem);
 });
 
 
@@ -17,11 +28,57 @@ function renderTextChannel(textChannel)
     listItem.id = 'textChannel_' + textChannel.id;
     listItem.classList.add('text-channel');
     
+    const textIcon = document.createElement('img');
+    listItem.append(textIcon);
+    textIcon.classList.add('icon-text-size');
+    textIcon.src = "../public/text-channel.svg";
+    textIcon.alt = " text-channel";
+
+    const listItemAnchor = document.createElement('a');
+    listItemAnchor.src = '#';
+    listItem.append(listItemAnchor);
+    listSpan = document.createElement('span');
+    listSpan.innerText = textChannel.name;
+    listItemAnchor.append(listSpan);
+    
+    const div = document.createElement('div');
+    div.classList.add('text-channel-icons');
+    listItem.append(div);
+
+    const anchorAddIcon = document.createElement('a');
+    anchorAddIcon.src = '#';
+    div.append(anchorAddIcon);
+    
+    const addIcon = document.createElement('img');
+    anchorAddIcon.append(addIcon);
+    addIcon.classList.add('icon-text-size');
+    addIcon.src = "../public/add_user.svg";
+    addIcon.alt = "add_user";
+    
+    const anchorsettingsIcon = document.createElement('a');
+    anchorsettingsIcon.src = '#';
+    div.append(anchorsettingsIcon);
+    
+    const settingsIcon = document.createElement('img');
+    anchorsettingsIcon.append(settingsIcon);
+    settingsIcon.classList.add('icon-text-size');
+    settingsIcon.src = "../public/settings.svg";
+    settingsIcon.alt = "settings";
+
+    return listItem;
+}
+
+function renderVoiceChannel(voiceChannel)
+{
+    const listItem = document.createElement('li');
+    listItem.id = 'voiceChannel_' + voiceChannel.id;
+    listItem.classList.add('text-channel');
+    
     const listItemAnchor = document.createElement('a');
     listItemAnchor.src = "#";
     listItem.append(listItemAnchor);
     listSpan = document.createElement('span');
-    listSpan.innerText = textChannel.name;
+    listSpan.innerText = voiceChannel.name;
     listItemAnchor.append(listSpan);
     
     const div = document.createElement('div');
