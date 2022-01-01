7f76e1f50ea92ac56d2f44aafb8071f2be4629bb
PedroAidos <up201706842@fe.up.pt>
2022-01-01 17:41:26 +0000

    anchor error fiexed

--- app/javascript/secondary-nav.js
+++ app/javascript/secondary-nav.js
@@ -27,27 +27,29 @@ function renderTextChannel(textChannel)
     const listItem = document.createElement('li');
     listItem.id = 'textChannel_' + textChannel.id;
     listItem.classList.add('text-channel');
+    const div = document.createElement('div');
+    listItem.append(div);
     
     const textIcon = document.createElement('img');
-    listItem.append(textIcon);
+    div.append(textIcon);
     textIcon.classList.add('icon-text-size');
     textIcon.src = "../public/text-channel.svg";
-    textIcon.alt = " text-channel";
+    textIcon.alt = "text-channel";
 
     const listItemAnchor = document.createElement('a');
-    listItemAnchor.src = '#';
-    listItem.append(listItemAnchor);
+    listItemAnchor.href = '#';
+    div.append(listItemAnchor);
     listSpan = document.createElement('span');
     listSpan.innerText = textChannel.name;
     listItemAnchor.append(listSpan);
     
-    const div = document.createElement('div');
-    div.classList.add('text-channel-icons');
-    listItem.append(div);
+    const div1 = document.createElement('div');
+    div1.classList.add('text-channel-icons');
+    listItem.append(div1);
 
     const anchorAddIcon = document.createElement('a');
-    anchorAddIcon.src = '#';
-    div.append(anchorAddIcon);
+    anchorAddIcon.href = '#';
+    div1.append(anchorAddIcon);
     
     const addIcon = document.createElement('img');
     anchorAddIcon.append(addIcon);
@@ -56,8 +58,8 @@ function renderTextChannel(textChannel)
     addIcon.alt = "add_user";
     
     const anchorsettingsIcon = document.createElement('a');
-    anchorsettingsIcon.src = '#';
-    div.append(anchorsettingsIcon);
+    anchorsettingsIcon.href = '#';
+    div1.append(anchorsettingsIcon);
     
     const settingsIcon = document.createElement('img');
     anchorsettingsIcon.append(settingsIcon);
@@ -75,7 +77,7 @@ function renderVoiceChannel(voiceChannel)
     listItem.classList.add('text-channel');
     
     const listItemAnchor = document.createElement('a');
-    listItemAnchor.src = "#";
+    listItemAnchor.heref = "#";
     listItem.append(listItemAnchor);
     listSpan = document.createElement('span');
     listSpan.innerText = voiceChannel.name;
@@ -86,7 +88,7 @@ function renderVoiceChannel(voiceChannel)
     listItem.append(div);
     
     const anchorAddIcon = document.createElement('a');
-    anchorAddIcon.src = '#';
+    anchorAddIcon.href = '#';
     div.append(anchorAddIcon);
     
     const addIcon = document.createElement('img');
@@ -96,7 +98,7 @@ function renderVoiceChannel(voiceChannel)
     addIcon.alt = "add_user";
     
     const anchorsettingsIcon = document.createElement('a');
-    anchorsettingsIcon.src = "#";
+    anchorsettingsIcon.href = "#";
     div.append(anchorsettingsIcon);
     
     const settingsIcon = document.createElement('img');
