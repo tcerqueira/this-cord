50f933858006b787b734d8a58e455cad153eb4aa
tcerqueira <tiago.cerqueira7777@gmail.com>
2022-01-09 16:18:51 +0000

    chat js rename

--- app/r/text-channel.php
+++ app/r/text-channel.php
@@ -10,7 +10,7 @@ require 'scripts/SendHeaders.php';
     <?php include "../components/head.php" ?>
     <link rel="stylesheet" href="../styles/chat.css">
     <link rel="stylesheet" href="../styles/sidebar.css">
-    <script src="../javascript/text-channel.js" defer></script>
+    <script src="../javascript/chat.js" defer></script>
     <script src="../javascript/guild.js" defer></script>
 </head>
 <body>
