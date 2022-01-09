fb72bde9a6a265df5684bc952f6fe789922989d2
tcerqueira <tiago.cerqueira7777@gmail.com>
2022-01-09 16:55:42 +0000

    refactor javascript structure

---
+++ app/javascript/text-channel.js
@@ -0,0 +1,19 @@
+const currentTextChannelId = document.getElementById('currentChannelId').dataset.channelId;
+
+render();
+async function render()
+{
+    try {
+        const [ myGuilds, channel ] = await Promise.all([
+            api.fetchMyGuilds(),
+            api.fetchTextChannel({ id: currentTextChannelId })
+        ]);
+        const members = await api.fetchGuildMembers({ id: channel.guild_id });
+
+        renderNav(myGuilds);
+        renderMembers(members);
+        renderChat(messages);
+    } catch (err) {
+        console.log(err);
+    }
+}
