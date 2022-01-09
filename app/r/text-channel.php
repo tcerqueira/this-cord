5298941541726517ccd3aed0d203617d992fab1a
tcerqueira <tiago.cerqueira7777@gmail.com>
2022-01-09 16:16:51 +0000

    turn chat into a component

--- app/r/text-channel.php
+++ app/r/text-channel.php
@@ -8,7 +8,7 @@ require 'scripts/SendHeaders.php';
 <html lang="en">
 <head>
     <?php include "../components/head.php" ?>
-    <link rel="stylesheet" href="../styles/text-channel.css">
+    <link rel="stylesheet" href="../styles/chat.css">
     <link rel="stylesheet" href="../styles/sidebar.css">
     <script src="../javascript/text-channel.js" defer></script>
     <script src="../javascript/guild.js" defer></script>
@@ -23,42 +23,7 @@ require 'scripts/SendHeaders.php';
         <?php include '../components/server-header.php'; ?>
         <?php include '../components/channel-header.php'; ?>
         <main class="main-container text-channel-container">
-            <div class="messages-container">
-                <!-- Empty element to reference to go back to bottom messages -->
-                <div id="back-to-bottom"></div>
-                <ol id="messages-list">
-                    <!-- <li id="message_2" class="message">
-                        <a href="#message_123">
-                            <div class="reply-preview"><span class="username">@tanso</span>: hello bro</div>
-                        </a>
-                        <h3 class="message-author">
-                            <div class="author-avatar">
-                                lou
-                            </div>
-                            <span>
-                                <span class="username">lou</span>
-                                <span class="message-date">Agora</span>
-                            </span>
-                        </h3>
-                        Hello mockmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
-                        <div class="message-options">
-                            <a href="#"><img src="../public/reply-svgrepo-com.svg" alt="reply-icon"></a>
-                            <a href="#"><img src="../public/trash-svgrepo-com.svg" alt="remove-icon"></a>
-                        </div>
-                    </li> -->
-                    <!-- FETCHED CLIENT-SIDE -->
-                </ol>
-            </div>
-            <div id="reply-container" class="reply-container">
-                    <span>Replying to <span id="replyingToUsername" class=username></span></span>
-                    <img id="cancel-reply-icon" src="../public/cancel-svgrepo-com.svg" alt="cancel-icon">
-            </div>
-            <div class="input-container">
-                <div class="text-input-container message-box">
-                    <span>+</span>
-                    <input type="text" name="message" id="message-input">
-                </div>
-            </div>
+            <?php include '../components/chat.php'; ?>
         </main>
         <?php include "../components/right-sidebar.php"; ?>
     </div>
