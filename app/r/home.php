5513ddd1eee5be641906b697d5fa799adad5b6a6
tcerqueira <tiago.cerqueira7777@gmail.com>
2022-01-10 00:09:16 +0000

    style user list

--- app/r/home.php
+++ app/r/home.php
@@ -20,8 +20,59 @@ require 'scripts/SendHeaders.php';
         <?php include '../components/nav-friends.php'; ?>
         <?php include '../components/server-header.php'; ?>
         <?php include '../components/home-topbar.php'; ?>
-        <main class="main-container">
-        
+        <main class="main-container home-container">
+            <h2>Online</h2>
+            <ul class="users-list">
+                <li class="user-item">
+                    <div class="icon-card icon-size-small">
+
+                    </div>
+                    <div class="user-item-info">
+                        <span class="user-item-username">Username</span>
+                        <span>Status</span>
+                    </div>
+                    <div class="user-item-options">
+                        <a href="#">
+                            <div data-tooltip="Message">
+                                <img src="../public/message-svgrepo-com.svg" alt="mensage-icon">
+                            </div>
+                        </a>
+                        <!-- <div data-tooltip="Options">
+                            <img src="../public/options-vertical-svgrepo-com.svg" alt="options-icon">
+                        </div> -->
+                    </div>
+                </li>
+                <li class="user-item">
+                    <div class="icon-card icon-size-small">
+
+                    </div>
+                    <div class="user-item-info">
+                        <span class="user-item-options">Username</span>
+                        <span>Status</span>
+                    </div>
+                    <div class="user-item-options">
+                        <a href="#">
+                            <div><img src="../public/message-svgrepo-com.svg" alt="mensage-icon"></div>
+                        </a>
+                        <div><img src="../public/options-vertical-svgrepo-com.svg" alt="options-icon"></div>
+                    </div>
+                </li>
+                <li class="user-item">
+                    <div class="icon-card icon-size-small">
+
+                    </div>
+                    <div class="user-item-info">
+                        <span class="user-item-options">Username</span>
+                        <span>Status</span>
+                    </div>
+                    <div class="user-item-options">
+                        <a href="#">
+                            <div><img src="../public/message-svgrepo-com.svg" alt="mensage-icon"></div>
+                        </a>
+                        <div><img src="../public/options-vertical-svgrepo-com.svg" alt="options-icon"></div>
+                    </div>
+                </li>
+            </ul>
         </main>
         <?php include "../components/invite-sidebar.php"; ?>
     </div>
