0c6ce09251b07383709ef77cb59186c4f49c2fe6
tcerqueira <tiago.cerqueira7777@gmail.com>
2022-01-10 23:46:57 +0000

    top bar frontend

--- app/components/home-topbar.php
+++ app/components/home-topbar.php
@@ -1,7 +1,10 @@
 <header class="page-header home-topbar-container shadow">
     <div class="tabs-container">
-        <div class="selected-tab-container">
-        </div>
+        <h2><button class="button topbar-tab selected-tab">Online</button></h2>
+        <div class="vertical-bar"></div>
+        <h2><button class="button topbar-tab">All</button></h2>
+        <div class="vertical-bar"></div>
+        <h2><button class="button topbar-tab">Pending</button></h2>
     </div>
     <div class="search-input-container topbar-search">
         <input type="text" id="topbar-search-input" placeholder="Search">
