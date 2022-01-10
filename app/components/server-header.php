94ec5e438ac5d12656ba89a06651272ec3fa2a09
tcerqueira <tiago.cerqueira7777@gmail.com>
2022-01-10 22:59:45 +0000

    server header on hover

--- app/components/server-header.php
+++ app/components/server-header.php
@@ -1,5 +1,5 @@
 <header class="nav-header shadow">
-    <h1>Server header</h1>
+    <h1 class="one-liner">Server header</h1>
     <div class="server-options-container">
         <img src="../public/add-user-symbol-of-interface-svgrepo-com.svg" alt="invite-member-icon">
         <img src="../public/settings.svg" alt="server-settings-icon">
