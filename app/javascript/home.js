f88a23780a60a48c8689eb8f47a1a2a3c3f49884
tcerqueira <tiago.cerqueira7777@gmail.com>
2022-01-12 19:24:03 +0000

    home page listing done

--- app/javascript/home.js
+++ app/javascript/home.js
@@ -1,41 +1,36 @@
+var friends = [];
 render();
 
 async function render()
 {
     try {
-        let [ myGuilds, friends, guildInvites ] = await Promise.all([
+        let [ myGuilds, friendsList, guildInvites ] = await Promise.all([
             api.fetchMyGuilds(),
             api.fetchFriends(),
             api.fetchGuildInvites()
         ]);
+        friends.push(...friendsList);
 
         renderNav(myGuilds);
-
-        let [ onlineList, allList, pendingList ] = [
-            friends.filter(f => f.invite_status === '1' && f.userstatus === '1'),
-            friends.filter(f => f.invite_status === '1'),
-            friends.filter(f => f.invite_status === '0')
-        ];
-        // console.log(friends);
 
         let currentTab = window.location.hash?.slice(1) || 'online';
         switchTab('online', currentTab);
         switch(currentTab) {
             case 'online':
-                renderUsersList(onlineList); break;
+                renderUsersList(getOnlineList(friends)); break;
             case 'all':
-                renderUsersList(allList); break;
+                renderUsersList(getAllList(friends)); break;
             case 'pending':
-                renderUsersList(pendingList); break;
+                renderUsersList(getPendingList(friends)); break;
         }
 
-        document.getElementById('onlineTabBtn').addEventListener('click', (evt) => {
+        document.getElementById('onlineTabBtn').addEventListener('click', () => {
             if(currentTab === 'online')
                 return;
             switchTab(currentTab, 'online');
             currentTab = 'online';
 
-            renderUsersList(onlineList);
+            renderUsersList(getOnlineList(friends));
         });
         
         document.getElementById('allTabBtn').addEventListener('click', () => {
@@ -44,7 +39,7 @@ async function render()
             switchTab(currentTab, 'all');
             currentTab = 'all';
 
-            renderUsersList(allList);
+            renderUsersList(getAllList(friends));
         });
         
         document.getElementById('pendingTabBtn').addEventListener('click', () => {
@@ -53,7 +48,7 @@ async function render()
             switchTab(currentTab, 'pending');
             currentTab = 'pending';
 
-            renderUsersList(pendingList);
+            renderUsersList(getPendingList(friends));
         });
     }
     catch (err) {
@@ -61,7 +56,19 @@ async function render()
     }
 }
 
-function renderUsersList(usersList) {
+function getOnlineList(friendsList) {
+    return friendsList.filter(f => f.invite_status === '1' && f.userstatus === '1');
+}
+
+function getAllList(friendsList) {
+    return friendsList.filter(f => f.invite_status === '1');
+}
+
+function getPendingList(friendsList) {
+    return friendsList.filter(f => f.invite_status === '0');
+}
+
+function renderUsersList(usersList, friendsList) {
     const list = document.getElementById('usersList');
     while (list.firstChild) {
         list.removeChild(list.firstChild);
@@ -71,88 +78,6 @@ function renderUsersList(usersList) {
             return;
         const item = createUserItem(user);
         list.append(item);
-
-        item.addEventListener('click', () => {
-            openModal('user-modal');
-            renderUserModal(user);
-        });
-
-        getAddIcon(item).addEventListener('click', async (evt) => {
-            evt.stopPropagation();
-            try {
-                await api.requestFriend({ id: user.id });
-                user.invite_status = '0';
-                user.request_sender = currentProfileId;
-                friends.push(user);
-                const newUserItem = createUserItem(user);
-                item.parentNode.replaceChild(newUserItem, item);
-            }
-            catch (err) {
-                console.log(err);
-            }
-        });
-
-        getMessageIcon(item).addEventListener('click', (evt) => {
-            evt.stopPropagation();
-        });
-
-        getRemoveIcon(item).addEventListener('click', (evt) => {
-            evt.stopPropagation();
-            openModal('confirmation-modal');
-            renderConfirmationModal('Are you sure you want to remove friend?', async () => {
-                try {
-                    await api.removeFriend({ id: user.id });
-                    user.invite_status = undefined;
-                    friends = friends.filter(f => f.id !== user.id);
-                    const newUserItem = createUserItem(user);
-                    item.parentNode.replaceChild(newUserItem, item);
-                }
-                catch (err) {
-                    console.log(err);
-                }
-            })
-        });
-
-        getAcceptIcon(item).addEventListener('click', async (evt) => {
-            evt.stopPropagation();
-            try {
-                const newFriend = await api.acceptFriendRequest({ id: user.id });
-                friends.push(newFriend);
-                const newUserItem = createUserItem(newFriend);
-                item.parentNode.replaceChild(newUserItem, item);
-            }
-            catch (err) {
-                console.log(err);
-            }
-        });
-
-        getDeclineIcon(item).addEventListener('click', (evt) => {
-            evt.stopPropagation();
-            try {
-                await api.removeFriend({ id: user.id });
-                user.invite_status = undefined;
-                friends = friends.filter(f => f.id !== user.id);
-                const newUserItem = createUserItem(user);
-                item.parentNode.replaceChild(newUserItem, item);
-            }
-            catch (err) {
-                console.log(err);
-            }
-        });
-
-        // cancel request icon on hover
-        getSentIcon(item).addEventListener('mouseover', (evt) => {
-            evt.stopPropagation();
-            getSentIcon(item).querySelector('img[alt=sent-icon]').style = 'display: none;'
-            getSentIcon(item).querySelector('img[alt=cancel-sent-icon]').style = 'display: inline-block;'
-        });
-
-        getSentIcon(item).addEventListener('mouseout', (evt) => {
-            evt.stopPropagation();
-            getSentIcon(item).querySelector('img[alt=sent-icon]').style = 'display: inline-block;'
-            getSentIcon(item).querySelector('img[alt=cancel-sent-icon]').style = 'display: none;'
-        });
-
     });
 }
 
@@ -160,6 +85,10 @@ function createUserItem(user) {
     const userItem = document.getElementById('userItemTemplate').cloneNode(true);
     userItem.style = '';
     userItem.removeAttribute('id');
+    userItem.addEventListener('click', () => {
+        openModal('user-modal');
+        renderUserModal(user);
+    });
 
     userItem.querySelector('.icon-card').style = `--icon-bg-color: ${user.theme_color};`;
     userItem.querySelector('.user-item-username').innerText = user.username;
@@ -183,6 +112,101 @@ function createUserItem(user) {
             }
             break;
     }
+    // event handlers
+    getAddIcon(userItem).addEventListener('click', async (evt) => {
+        evt.stopPropagation();
+        try {
+            await api.requestFriend({ id: user.id });
+            user.invite_status = '0';
+            user.request_sender = currentProfileId;
+            friends.push(user);
+            const newUserItem = createUserItem(user);
+            userItem.parentNode.replaceChild(newUserItem, userItem);
+        }
+        catch (err) {
+            console.log(err);
+        }
+    });
+
+    getMessageIcon(userItem).addEventListener('click', (evt) => {
+        evt.stopPropagation();
+    });
+
+    getRemoveIcon(userItem).addEventListener('click', (evt) => {
+        evt.stopPropagation();
+        openModal('confirmation-modal');
+        renderConfirmationModal('Are you sure you want to remove friend?', async () => {
+            try {
+                await api.removeFriend({ id: user.id });
+                user.invite_status = undefined;
+                friends = friends.filter(f => f.id !== user.id);
+                const newUserItem = createUserItem(user);
+                userItem.parentNode.replaceChild(newUserItem, userItem);
+            }
+            catch (err) {
+                console.log(err);
+            }
+            finally {
+                closeModal();
+            }
+        })
+    });
+
+    getAcceptIcon(userItem).addEventListener('click', async (evt) => {
+        evt.stopPropagation();
+        try {
+            const newFriend = await api.acceptFriendRequest({ id: user.id });
+            friends = friends.map(f => {
+                if(f.id === user.id)
+                    f = newFriend;
+                return f;
+            });
+            const newUserItem = createUserItem(newFriend);
+            userItem.parentNode.replaceChild(newUserItem, userItem);
+        }
+        catch (err) {
+            console.log(err);
+        }
+    });
+
+    getDeclineIcon(userItem).addEventListener('click', async (evt) => {
+        evt.stopPropagation();
+        try {
+            await api.removeFriend({ id: user.id });
+            user.invite_status = undefined;
+            friends = friends.filter(f => f.id !== user.id);
+            document.getElementById('usersList').removeChild(userItem);
+        }
+        catch (err) {
+            console.log(err);
+        }
+    });
+    // handler for cancelling request
+    getSentIcon(userItem).addEventListener('click', async (evt) => {
+        evt.stopPropagation();
+        try {
+            await api.removeFriend({ id: user.id });
+            user.invite_status = undefined;
+            friends = friends.filter(f => f.id !== user.id);
+            const newUserItem = createUserItem(user);
+            userItem.parentNode.replaceChild(newUserItem, userItem);
+        }
+        catch (err) {
+            console.log(err);
+        }
+    });
+    // cancel request icon on hover
+    getSentIcon(userItem).addEventListener('mouseover', (evt) => {
+        evt.stopPropagation();
+        getSentIcon(userItem).querySelector('img[alt=sent-icon]').style = 'display: none;'
+        getSentIcon(userItem).querySelector('img[alt=cancel-sent-icon]').style = 'display: inline-block;'
+    });
+
+    getSentIcon(userItem).addEventListener('mouseout', (evt) => {
+        evt.stopPropagation();
+        getSentIcon(userItem).querySelector('img[alt=sent-icon]').style = 'display: inline-block;'
+        getSentIcon(userItem).querySelector('img[alt=cancel-sent-icon]').style = 'display: none;'
+    });
 
     return userItem;
 }
