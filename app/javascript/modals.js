373ca00c64ee4835cc5ce83295c9a886644395ea
tcerqueira <tiago.cerqueira7777@gmail.com>
2022-01-13 23:11:29 +0000

    fix testing modal

--- app/javascript/modals.js
+++ app/javascript/modals.js
@@ -68,8 +68,6 @@ function openConfirmationModal(message, callbackFn)
     newButton.addEventListener('click', callbackFn);
 }
 
-openConfirmationModal('Are you sure you want to delete this user?', _ => console.log('sheeesh'));
-
 function openModal(elemId)
 {
     modalPane.style.display = 'flex';
