e630c97dc21d46b7f5103c5d3276f6e989cf5ae6
tcerqueira <tiago.cerqueira7777@gmail.com>
2022-01-21 00:09:31 +0000

    open guild invite page layout

---
+++ app/r/invite-to-guild.php
@@ -0,0 +1,28 @@
+<?php
+require 'scripts/StartSession.php';
+require 'scripts/NotAuthenticatedCheck.php';
+require 'scripts/SendHeaders.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include "../components/head.php" ?>
+    <link rel="stylesheet" href="../styles/invite-to-guild.css">
+    <script src="../javascript/invite-to-guild.js" defer></script>
+</head>
+<body>
+
+    <main>
+        <div class="invite-to-guild-container rounded-container">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2zm0 18c-4.411 0-8-3.589-8-8s3.589-8 8-8 8 3.589 8 8-3.589 8-8 8z"></path><path d="M9.999 13.587 7.7 11.292l-1.412 1.416 3.713 3.705 6.706-6.706-1.414-1.414z"></path></svg>
+            <div class="invite-message">
+                <h1>Welcome aboard!</h1>
+                <a href="#">Go explore guild</a>
+            </div>
+            <h1></h1>
+        </div>
+    </main>
+
+</body>
+</html>
