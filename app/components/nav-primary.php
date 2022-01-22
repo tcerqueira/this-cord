1445183278975e388bce11e079fa19d2e2c8927f
tcerqueira <tiago.cerqueira7777@gmail.com>
2022-01-22 02:55:59 +0000

    log out icon

--- app/components/nav-primary.php
+++ app/components/nav-primary.php
@@ -1,15 +1,19 @@
 <nav class="primary-nav">
-    <a href="home.php">
-        <div class="icon-size-medium icon-card side-card" data-tooltip="Home">
-            <div><span>Home</span></div>
-            <!-- <span class="tooltip">Home</span> -->
+    <div>
+        <a href="home.php">
+            <div id="homeIcon" class="icon-size-medium icon-card side-card" data-tooltip="Home">
+                <div><span>Home</span></div>
+                <!-- <span class="tooltip">Home</span> -->
+            </div>
+        </a>
+        <hr/>
+        <div id="guilds-container" class="guilds-container">            
         </div>
-    </a>
-    <hr/>
-    <div id="guilds-container" class="guilds-container">
-
+        <div id="createGuildNavIcon" class="icon-size-medium icon-card side-card" data-tooltip="Create Server">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19 11h-6V5h-2v6H5v2h6v6h2v-6h6z"></path></svg>
+        </div>
     </div>
-    <div id="createGuildNavIcon" class="icon-size-medium icon-card side-card" data-tooltip="Create Server">
-        +
+    <div id="logoutIcon" class="icon-size-medium icon-card side-card" data-tooltip="Log out">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="m2 12 5 4v-3h9v-2H7V8z"></path><path d="M13.001 2.999a8.938 8.938 0 0 0-6.364 2.637L8.051 7.05c1.322-1.322 3.08-2.051 4.95-2.051s3.628.729 4.95 2.051 2.051 3.08 2.051 4.95-.729 3.628-2.051 4.95-3.08 2.051-4.95 2.051-3.628-.729-4.95-2.051l-1.414 1.414c1.699 1.7 3.959 2.637 6.364 2.637s4.665-.937 6.364-2.637c1.7-1.699 2.637-3.959 2.637-6.364s-.937-4.665-2.637-6.364a8.938 8.938 0 0 0-6.364-2.637z"></path></svg>
     </div>
 </nav>
