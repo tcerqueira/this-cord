c07a023fde86dfeea9c970a13d812e271c2d6e92
tcerqueira <tiago.cerqueira7777@gmail.com>
2022-02-09 01:03:00 +0000

    meta tags

--- app/r/scripts/head-outside.php
+++ app/r/scripts/head-outside.php
@@ -1,8 +1,25 @@
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!-- Primary Meta Tags -->
 <title>This-Cord</title>
 <link rel="icon" type="image/svg+xml" href="../public/wolf-logo.svg">
+<meta name="title" content="This-Cord - Discord of Wish">
+<meta name="description" content="Connect with friends, build communities and chat. Discord clone with education purposes, don't sue me. SUS">
+
+<!-- Open Graph / Facebook -->
+<meta property="og:type" content="website">
+<meta property="og:url" content="https://metatags.io/">
+<meta property="og:title" content="This-Cord - Discord of Wish">
+<meta property="og:description" content="Connect with friends, build communities and chat. Discord clone with education purposes, don't sue me. SUS">
+<meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
+
+<!-- Twitter -->
+<meta property="twitter:card" content="summary_large_image">
+<meta property="twitter:url" content="https://metatags.io/">
+<meta property="twitter:title" content="This-Cord - Discord of Wish">
+<meta property="twitter:description" content="Connect with friends, build communities and chat. Discord clone with education purposes, don't sue me. SUS">
+<meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
 <!-- CSS -->
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
